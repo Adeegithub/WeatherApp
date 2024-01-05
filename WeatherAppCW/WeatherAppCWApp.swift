79d4f37d2933cf9abb01b21b6bd4002bdf0bc672
Adeegithub <arunodagunawardana@gmail.com>
2024-01-05 14:52:27 +0530

    Initial Commit

---
+++ WeatherAppCW/WeatherAppCWApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppCWApp.swift
+//  WeatherAppCW
+//
+//  Created by Adeesha Arunoda Gunawardana on 5/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppCWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
